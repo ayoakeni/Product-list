0daa8611d7f5604701c9013ccc987ec3ab8c8d2f
ayo akeni <ayoakeni64@gmail.com>
2024-04-06 20:38:53 +0100

    update

--- script.js
+++ script.js
@@ -130,52 +130,6 @@ downloadButton.addEventListener('click', () => {
   });  
 });
 
-// Swiper slide
-var swiper = new Swiper(".mySwiper", {
-  loop: true,
-  autoplay: {
-    delay: 3000,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
-    600: {
-      slidesPerView: 1.5,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    941: {
-      slidesPerView: 1.4,
-      spaceBetween: 30,
-    },
-    970: {
-      slidesPerView: 1.4,
-      spaceBetween: 30,
-    },
-    1024: {
-      slidesPerView: 3.4,
-      spaceBetween: 30,
-    },
-    1349: {
-      slidesPerView: 3,
-      spaceBetween: 130,
-    },
-    1440: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-  },
-});
-
 // const download = document.getElementById('download');
 
 // download.addEventListener('click', ()=> {
