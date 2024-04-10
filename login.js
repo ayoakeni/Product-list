fadacbd71ff80229d5b8da893063aa1cf0847bda
ayo akeni <ayoakeni64@gmail.com>
2024-04-10 18:45:53 +0100

    update

--- login.js
+++ login.js
@@ -1,28 +1,3 @@
-// // Get the login form element
-// const loginForm = document.getElementById('loginForm');
-
-// // Add event listener to the form submit button
-// loginForm.addEventListener('submit', function(event) {
-//   event.preventDefault(); // Prevent the form from submitting
-
-//   // Get the entered password
-//   const enteredPassword = document.getElementById('password').value;
-
-//   // Get the saved password from localStorage
-//   const savedPassword = localStorage.getItem('password');
-
-//   // Check if the entered password matches the saved password
-//   if (enteredPassword === savedPassword) {
-//     alert('Login successful!');
-//     // Redirect to the account page
-//     window.location.href = 'account.html';
-//   } else {
-//     alert('Incorrect password. Please try again.');
-//     // Clear the password field
-//     document.getElementById('password').value = '';
-//   }
-// });
-
 document.addEventListener('DOMContentLoaded', function() {
   const splashScreen = document.getElementById('splash-screen');
   const loginContainer = document.querySelector('.login-container');
