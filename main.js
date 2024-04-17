27c190a757fbd9c59fc4c83341dcd610723b88d4
ayo akeni <ayoakeni64@gmail.com>
2024-04-17 15:21:36 +0100

    update

--- main.js
+++ main.js
@@ -174,42 +174,45 @@ searchInput.addEventListener('input', () => {
 
 // Barcode scanning
 const barcode = document.getElementById('barcode');
-let resultElement = document.getElementById('result');
+const resultElement = document.getElementById('result');
 
-barcode.addEventListener('keydown', (event) => {
-  if (event.key >= '0' && event.key <= '9' && barcode.value.length === 12) {
-    // Only allow numbers and check length
+barcode.addEventListener('input', () => {
+  let cleanedValue = barcode.value.replace(/\D/g, ''); // Remove non-numeric characters
+  barcode.value = cleanedValue; // Update the input value
+  
+  if (barcode.value.length === 13) {
     submitBarcode();
-    // display result
     resultElement.style.display = 'flex';
-  }else {
+    contentBox.style.display = "none";
+  } else {
     resultElement.style.display = 'none';
+    contentBox.style.display = "flex";
   }
 });
 
 function submitBarcode() {
-  const barcode = barcodeInput.value;
-  var apiKey = 'YOUR_API_KEY'; // Replace with your API key
-  fetch(`https://api.upcitemdb.com/prod/trial/lookup?upc=${barcode}&apikey=${apiKey}`)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    if (data && data.items && data.items.length > 0) {
-      var product = data.items[0];
-      resultElement.innerText = `
-        Product Name: ${product.title}\n
-        Brand: ${product.brand}\n
-        Description: ${product.description}
-      `;
-    } else {
-      resultElement.innerText = 'Product not found';
-    }
-  })
-  .catch(error => {
-    console.error('Error:', error);
-    resultElement.innerText = "Error: " + error.message;
-  });
+  const apiKey = 'YOUR_API_KEY'; // Replace with your API key
+  fetch(`https://api.upcitemdb.com/prod/trial/lookup?upc=${barcode.value}&apikey=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      if (data && data.items && data.items.length > 0) {
+        const product = data.items[0];
+        resultElement.innerText = `
+          Product Name: ${product.title}\n
+          Brand: ${product.brand}\n
+          Description: ${product.description}
+        `;
+      } else {
+        resultElement.innerText = 'Product not found';
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      resultElement.innerText = "Error: " + error.message;
+    });
 }
+
 const addToCartButtons = document.querySelectorAll('.content-box button');
 const itemsNo = document.getElementById('items-no');
 const totalPriceElement = document.querySelector('.total span');
